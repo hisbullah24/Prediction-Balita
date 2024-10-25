09d0630b80ad25efdbbf03aafd4b20375cb4d7ed
Said Ali Nuryudha Hisbullah <159878831+hisbullah24@users.noreply.github.com>
2024-10-25 15:21:40 +0700

    Update Streamlit_balita.py

--- Streamlit_balita.py
+++ Streamlit_balita.py
@@ -9,7 +9,10 @@ gender = st.radio(
     index=None,
 )
 
-st.write("You selected:", gender)
+st.write("Gender anakmu:", gender)
 
 umur = st.slider("Berapa bulan umur anak anda?", 0, 60, 30)
 st.write("Anak anda berumur ", umur, "Bulan")
+
+tinggi = st.slider("Berapa tinggi anak anda?", 0, 150, 0)
+st.write("Tinggi anak anda ", tinggi, "cm")
