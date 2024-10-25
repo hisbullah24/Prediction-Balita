d9ad7fb789ec04fca78f624e3a7f8f786be21626
Said Ali Nuryudha Hisbullah <159878831+hisbullah24@users.noreply.github.com>
2024-10-25 16:15:17 +0700

    Update Streamlit_balita.py

--- Streamlit_balita.py
+++ Streamlit_balita.py
@@ -4,7 +4,7 @@ import pickle
 st.title('Prediction Stunting Toddler')
 
 st.set_page_config(
-    page_title="Multiple App"
+    page_title="Multiple App",
 )
 
 st.sidebar.success("Select a demo above.")
