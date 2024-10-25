1bb7a454aa136b7b8905726b6cfadb3392148da4
Said Ali Nuryudha Hisbullah <159878831+hisbullah24@users.noreply.github.com>
2024-10-25 15:09:46 +0700

    Update Streamlit_balita.py
    commit 2

--- Streamlit_balita.py
+++ Streamlit_balita.py
@@ -2,3 +2,11 @@ import streamlit as st
 import pickle
 
 st.title('Prediction Stunting Toddler')
+
+gender = st.radio(
+    "What's your kids gender",
+    ["Laki-laki :male:", "Perempuan :female:"],
+    index=None,
+)
+
+st.write("You selected:", gender)
