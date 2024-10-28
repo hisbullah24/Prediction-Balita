1c3a7c85a0bfc35a6f5c85a07381a6b83ae83b15
Said Ali Nuryudha Hisbullah <159878831+hisbullah24@users.noreply.github.com>
2024-10-28 15:27:12 +0700

    Update 2_Grafik.py

--- pages/2_Grafik.py
+++ pages/2_Grafik.py
@@ -1,16 +1,1 @@
-import streamlit as st
-import pandas as pd
 
-uploaded_file = st.file_uploader("./data_balita.csv", type="csv")
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-
-    fig = px.sunburst(
-        df,
-        path=['jenis_kelamin', 'status_gizi'],
-        values=None,                          
-        color_discrete_sequence=['#f66095', '#2bcdc1']
-    )
-
-    st.plotly_chart(fig)
